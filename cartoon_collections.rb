f52c0df8116ec8a23199b83d8be0ec9c71fb4d98
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-07-26 07:49:53 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  dwarves.each.with_index(1)do |dwarf, index|
+def roll_call_dwarves(dwarf)# code an argument here
+ dwarves.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
 end
 
 def summon_captain_planet# code an argument here
