fdc14c76e0a5c3d36fe271ad1203010807adfe59
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-07-26 07:54:31 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,10 +12,10 @@ def long_planeteer_calls(planet_calls)
   planet_calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese(potentially_cheesy_items)
+def find_the_cheese(cheesy_items)
   cheeses = %w(gouda cheddar camembert)
 
-  potentially_cheesy_items.find do |maybe_cheese|
+  cheesy_items.find do |maybe_cheese|
     cheeses.include?(maybe_cheese)
   end
 end
